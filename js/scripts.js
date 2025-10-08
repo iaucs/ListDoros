58f558b3583c3805fdea8c68bb722fb814bbfd0f
AmirHossein Shirazi <ashzih2@gmail.com>
2025-10-08 13:06:42 +0330

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const jsonDataUrl = "/json/4032.json";
+    const jsonDataUrl = "/json/4041.json";
     let jsonData = [];
     const pageSize = 20;
     let currentPage = 1;
